0ea81598dcc2bd5f445b918aa5b791a6d07f135b
roadprimus <roadprimus@yandex.ru>
2020-01-24 00:41:51 +0300

    Создан модуль (main_module.py), который с помощью контекстных менеджеров из двух файлов читает и на их основании создаёт новый файл. Написаны тесты на этот модуль (test_main_module.py).

---
+++ dz4/settings.py
@@ -0,0 +1,8 @@
+"""Модуль в котором хранятся настройки для пакета dz4."""
+
+
+PATH_TO_USERS_JSON = 'users.json'
+PATH_TO_BOOKS_CSV = 'books.csv'
+
+PATH_TO_RESULT = 'result.json'
+PATH_TO_RESULT_TEST = 'test_result.json'
