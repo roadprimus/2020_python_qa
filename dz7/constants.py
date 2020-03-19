0b7f5cbd17245d98982f06e081db76017d586a11
roadprimus <59182906+roadprimus@users.noreply.github.com>
2020-03-20 00:41:27 +0300

    ДЗ 7. Пункт 2. (#11)

---
+++ dz7/constants.py
@@ -0,0 +1,26 @@
+"""Константы для dz7."""
+
+
+class BrowserName:
+    """Наименования браузеров.
+
+    В классе хранятся наименования браузеров, которые пользователь
+    может использовать для тестирования приложения.
+
+    Attributes:
+        CHROME - 'chrome'
+        FIREFOX - 'firefox'
+    """
+
+    CHROME = 'chrome'
+    FIREFOX = 'firefox'
+
+
+BROWSER_CHOICE_ERROR = "Выберите chrome или firefox"
+H1 = 'h1'
+H1_TEXT = 'Your Store'
+OPENCART_ERROR = 'Полученный сайт не соответствует ожидаемому.'
+
+HEADLESS = 'headless'
+
+OPENCART_PATH = 'http://127.0.0.1/opencart/'
